24f8edab436a50653c9b7e3711c9f161ad016e5e
Maximilian Grundke <maximiliangrundke@googlemail.com>
2014-06-09 21:05:59 +0200

    Fix imports

--- src/com/parnswir/unmp/media/FileCrawlerThread.java
+++ src/com/parnswir/unmp/media/FileCrawlerThread.java
@@ -2,13 +2,10 @@ package com.parnswir.unmp.media;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
 
 import android.database.sqlite.SQLiteDatabase;
 
